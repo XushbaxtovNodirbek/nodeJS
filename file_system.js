7327d1eefa29cb2188ea68b425fc011e42bf49a4
nodirbek <nodirbek.devoloper@gmail.com>
2023-11-23 02:35:02 +0500

    module fs

--- file_system.js
+++ file_system.js
@@ -5,3 +5,30 @@ const path = require('path')
 
 // bismillah 
 
+// Create folder
+
+// fs.mkdir(path.join(__dirname,'templates'),err => {
+//     if(err) throw new Error(err)
+//     console.log('Folder created...')
+// })
+
+// Create and write to file
+
+// fs.writeFile(path.join(__dirname,'mynotes.txt'),'Create txt file',err => {
+//     if(err) throw new Error(err)
+
+//     console.log('File created...')
+
+//     fs.appendFile(path.join(__dirname,'mynotes.txt'),' and append text',err => {
+//         if(err) throw new Error(err)
+//         console.log('File appended...')
+//     })
+// })
+
+// Read file
+
+fs.readFile(path.join(__dirname,'utf-8','mynotes.txt'),(err,data) => {
+    if(err) throw new Error(err)
+    console.log(data.toString()) // if not use utf-8, data will be buffer
+    console.log(data)
+})
