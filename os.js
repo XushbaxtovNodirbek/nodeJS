19d79ef1cf7f045dd77d711db972dc7c7c785c71
nodirbek <nodirbek.devoloper@gmail.com>
2023-11-23 14:47:44 +0500

    module os

---
+++ os.js
@@ -0,0 +1,18 @@
+
+const os = require('os');
+
+// platform
+console.log(os.platform());
+
+// information
+console.log(os.cpus());
+
+// arch
+console.log(os.arch());
+
+// memory
+console.log(os.freemem());
+console.log(os.totalmem());
+
+// entry point
+console.log(os.homedir());
